3f70b15f05e258ce87c392c1583904a0667a1020
smdbz <github.commodore391@passmail.net>
2026-03-02 15:27:10 +0000

    Removed `BaseTutor`, `TutorPipeline`, and all associated core files to streamline project structure.

--- src/ds_tutor/__init__.py
+++ src/ds_tutor/__init__.py
@@ -1,8 +1,8 @@
-from .core.pipeline import TutorPipeline
+from .core import DsTutor
 from .tutor.imputer_tutor import ImputerTutor
 from .tutor.scaling_tutor import ScalingTutor
 
-__all__ = ["TutorPipeline", "ImputerTutor", "ScalingTutor", "hello"]
+__all__ = ["DsTutor", "ImputerTutor", "ScalingTutor", "hello"]
 
 def hello() -> str:
     return "Hello from ds-tutor!"
